ebad6d223e51e5db67d398159118e0f8388cad4f
DmytroPshen <mailforajob49@gmail.com>
2023-06-29 13:28:36 +0300

    translate some text in english

--- index.php
+++ index.php
@@ -1,22 +1,29 @@
 <?php
-// Задача із сторінки:
-//https://code.mu/ru/php/book/prime/basis/variables-usage/
+// Exercise from page:
+// https://code.mu/ru/php/book/prime/basis/variables-usage/
 
     /*Создайте переменную $num и присвойте ей значение 123.
     Выведите значение этой переменной на экран с помощью команды echo. */
-// Вирішення:
+// Solution:
 //$num = 123;
 //echo $num;
 
-// Задача із сторінки:
-//https://code.mu/ru/php/book/prime/basis/variables-values-changing/
+// Exercise from page:
+// https://code.mu/ru/php/book/prime/basis/variables-values-changing/
 
     /*Создайте переменную $a. Запишите в нее значение 10, выведите его на экран.
     Затем запишите в нее значение 20, выведите его на экран. */
 
-//Вирішення:
-$a = 10;
-echo $a;
+// Solution:
+//$a = 10;
+//echo $a;
+//
+//$a = 20;
+//echo $a;
 
-$a = 20;
-echo $a;
+// Exercise from page:
+// https://code.mu/ru/php/book/prime/basis/math-operations-numbers/
+    /*Создайте переменную $a. Запишите в нее сумму чисел 1, 2 и 3.
+    С помощью команды echo выведите на экран содержимое переменной $a. */
+// Solution:
+
