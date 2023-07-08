e4fdd2e40dec953a2e3304bab327a970cdac7095
DmytroPshen <mailforajob49@gmail.com>
2023-07-08 17:24:09 +0300

    08.07.23 task php fractions

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
+// Exercise from page:
+// https://code.mu/ru/php/book/prime/basis/fractions/
 
+    /*Запишите в переменную $a число 1.5, а в переменную $b - число 0.75.
+    Найдите сумму значений этих переменных и выведите ее на экран.
+    */
+// Solution:
+$a = 1.5;
+$b = 0.75;
+$sum = $a + $b;
+echo $sum;
