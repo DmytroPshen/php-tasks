4d97d88446b78f7395e05d7c4980fa5b614b3ac1
DmytroPshen <mailforajob49@gmail.com>
2023-07-08 17:28:10 +0300

    08.07.23 php negative numbers task 1

--- index.php
+++ index.php
@@ -6,7 +6,18 @@
     Найдите сумму значений этих переменных и выведите ее на экран.
     */
 // Solution:
-$a = 1.5;
-$b = 0.75;
-$sum = $a + $b;
-echo $sum;
+//$a = 1.5;
+//$b = 0.75;
+//$sum = $a + $b;
+//echo $sum;
+
+// Exercise from page:
+// https://code.mu/ru/php/book/prime/basis/negative-numbers/
+
+    /*№1
+     Создайте переменную $a со значением -100.
+    Выведите это значение на экран.
+    */
+// Solution:
+$a = -100;
+echo $a;
