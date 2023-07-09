9211e00db061836275732bec3a36d5ec725cad6a
DmytroPshen <mailforajob49@gmail.com>
2023-07-09 14:31:25 +0300

    09.07.23 php html tags task 2

--- index.php
+++ index.php
@@ -8,5 +8,20 @@
     */
 
 // Solution:
-$str = 'Hello word';
-echo '<i>' . $str . '</i>';
+//$str = 'Hello word';
+//echo '<i>' . $str . '</i>';
+
+    /* №2
+    С помощью тега <br> выведите на экран столбец чисел от 1 до 9.
+    */
+
+// Solution:
+echo '1<br>';
+echo '2<br>';
+echo '3<br>';
+echo '4<br>';
+echo '5<br>';
+echo '6<br>';
+echo '7<br>';
+echo '8<br>';
+echo '9<br>';
