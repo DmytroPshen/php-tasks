7c7ff6722b0621f2e1f6f89688147c0ed1b7adf7
DmytroPshen <mailforajob49@gmail.com>
2023-07-09 14:05:42 +0300

    09.07.23 php string task1

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
+//Date: 09.07.2023.
 // Exercise from page:
+// https://code.mu/ru/php/book/prime/basis/strings-concatenation/
+
+    /* №1
+    Создайте переменную $str и присвойте ей строку 'hello'.
+    Выведите значение этой переменной на экран.
+    */
+
+// Solution:
+$str = 'hello';
+echo $str;
 
