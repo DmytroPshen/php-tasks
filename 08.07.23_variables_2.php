5b7c2a6fef83b8f35de821826d6a2daccaaac551
DmytroPshen <mailforajob49@gmail.com>
2023-07-09 14:00:42 +0300

    renamed files and create new file index.php

---
+++ 08.07.23_variables_2.php
@@ -0,0 +1,61 @@
+<?php
+// Exercise from page:
+// https://code.mu/ru/php/book/prime/basis/fractions/
+
+    /*Запишите в переменную $a число 1.5, а в переменную $b - число 0.75.
+    Найдите сумму значений этих переменных и выведите ее на экран.
+    */
+
+// Solution:
+//$a = 1.5;
+//$b = 0.75;
+//$sum = $a + $b;
+//echo $sum;
+
+// Exercise from page:
+// https://code.mu/ru/php/book/prime/basis/negative-numbers/
+
+    /*№1
+     Создайте переменную $a со значением -100.
+    Выведите это значение на экран.
+    */
+
+// Solution:
+//$a = -100;
+//echo $a;
+
+    /*№2
+    Создайте переменную $a, запишите в нее какое-нибудь положительное или отрицательное число.
+    Выведите на экран это число с обратным знаком.
+    */
+
+//Solution:
+//$a = 7;
+//$a = -8;
+//echo -$a;
+
+// Exercise from page:
+// https://code.mu/ru/php/book/prime/basis/division-remainder/
+
+    /*
+     Дана переменная $a со значением 10 и переменная $b со значением 3.
+     Найдите остаток от деления $a на $b.
+     */
+
+// Solution:
+//$a = 10;
+//$b = 3;
+//$res = $a%$b;
+//echo $res;
+
+// Exercise from page:
+// https://code.mu/ru/php/book/prime/basis/numbers-powering/
+
+    /*
+     Возведите число 2 в 10-ю степень. Выведите результат на экран.
+     */
+
+// Solution:
+$a = 2;
+$b = 10;
+echo $a ** $b;
