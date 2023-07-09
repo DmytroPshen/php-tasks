66d0e6b80feb7096197b12bcd472091a05363233
DmytroPshen <mailforajob49@gmail.com>
2023-07-09 14:25:46 +0300

    09.07.23 php tags task 1

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+//Date: 09.07.2023.
+// Exercise from page:
+// https://code.mu/ru/php/book/prime/basis/tags/
+
+    /* №1
+    С помощью тега <i> выведите на экран курсивный текст.
+    */
+
+// Solution:
+$str = 'Hello word';
+echo '<i>' . $str . '</i>';
